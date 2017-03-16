8022544b35f4e14dc99f617a96bc268194cc3521
Karen Kepner <kkepner14@gmail.com>
2017-03-16 14:02:50 -0600

    update readme, pseudo code js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-###Make the Number Guesser Game
+### Make the Number Guesser Game   
+
+Build the HTML and CSS.
+
+Add Javascript to make the gamw happen.
--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 // great javascript ideas
+//generate a random number between 1-100 as the answer
+//collect an input guess from the user from the input box
+//compare the guess to the answer
+//return info about the guess: too high, too low, win!
+//place the guess in the "last guess" div
+//clear the input field and disable it.
+//
