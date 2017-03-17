bb60d275ea62165f172d2c4229bd5bd20de2f18d
Karen Kepner <kkepner14@gmail.com>
2017-03-16 22:04:47 -0600

    Add hover states and disabled color change

--- js/main.js
+++ js/main.js
@@ -1,14 +1,45 @@
 // great javascript ideas
-//generate a random number between 1-100 as the answer
+//x generate a random number between 1-100 as the answer
 //collect an input guess from the user from the input box
 //compare the guess to the answer
 //return info about the guess: too high, too low, win!
 //place the guess in the "last guess" div
 //clear the input field and disable it.
-//
+//state functions at top
+
  function generateRandomNumber() {
-	randomAnswer = Math.floor(Math.random()*100);
-	console.log(randomAnswer);
+	return Math.floor(Math.random()*100+1);
  }
 
-generateRandomNumber();
+ function evaluateGuess(guess, answer) {
+   if (guess === answer) {
+     return 'Boom! ' + answer;
+   } else if (guess > answer){
+     return 'Your guess was ' + guess + ' is too high. The answer is ' + answer;
+   } else {
+     return 'Your guess was ' + guess + ' is too low. The answer is ' + answer;
+   }
+ }
+
+var submitButton = document.getElementById('submit-guess');
+
+//game loop, well named functions in the clicker
+submitButton.addEventListener("click", function(){
+  var number = document.getElementById('user-guess').value;
+  var answer = generateRandomNumber();
+  var guess = parseInt(number);
+  //display last guess//clear input field
+  //deactivate submit
+  console.log(evaluateGuess(guess, answer));
+})
+
+//in the click!
+// generateRandomNumber()
+// get guesscomapare guess()
+// clear field()
+
+
+//junk pile
+// debugger;
+// event.preventDefault();
+// console.log(guess.innerText);
